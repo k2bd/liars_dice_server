33bb724148d038d308ae8f9e029465fd4d8b6f4e
Kevin Duff <kevinkelduff@gmail.com>
2018-06-30 17:37:59 +0100

    Some updatess to API

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 
 from pymongo import MongoClient
-from flask import Flask, request
+from flask import Flask, jsonify,json,request
+import random
 
 application = Flask(__name__)
-client = MongoClient('localhost:27017')
-db = client.liarsdice
+
+# TODO: Statelessify this with mongo
+#client = MongoClient('localhost:27017')
+#db = client.liarsdice
+
+class Player:
+    def __init__(self,name,player_id):
+        self.player_name = name
+        self.player_id = player_id
+
+class LiarsDiceGame:
+    def __init__(self, game_id):
+        self.game_id = game_id
+        self.players = [] # Players, in turn order
+        self.current_turn = 0
+        self.current_bet = [0,0] # number, rank
+
+    def addPlayer(self,Player):
+        self.players.append(Player)
+        random.shuffle(self.players)
+
+games = set()
 
 @application.route("/start/<gameid>",methods=["GET"])
 def startgame(gameid):
@@ -15,18 +36,17 @@ def startgame(gameid):
 @application.route("/config/<gameid>",methods=["GET","POST","DELETE"])
 def config(gameid):
     if request.method == "POST":
-        if db.Games.find_one({"game_id":gameid}).count == 0:
+        if gameid not in games:
             # Game does not yet exist - create it
-            game = 
-            {
-                'game_id' : gameid,
-                'players' : [],
-            }
-            db.games.insert_one(game)
+            games.insert(LiarsDiceGame(gameid))
 
         # Now insert this player
         try:
-            db.Games.
+            player_name = request.json['name']
+            player_id   = request.json['id']
+            games[gameid].addPlayer(Player(player_name,player_id))
+        except Exception,e:
+            print(e)
         
     if request.method == "GET":
         # Get game state
