64e117e74c044f55c14b80083900667bfb841b86
Kevin Duff <kevinkelduff@gmail.com>
2018-06-30 18:27:27 +0100

    Some updatess to API

--- app.py
+++ app.py
@@ -14,53 +14,71 @@ class Player:
     def __init__(self,name,player_id):
         self.player_name = name
         self.player_id = player_id
+        self.dice_left = 5
+        self.dice = [0,0,0,0,0,0]
 
 class LiarsDiceGame:
     def __init__(self, game_id):
         self.game_id = game_id
-        self.players = [] # Players, in turn order
+        self.player_names = set()
+        self.players = []
         self.current_turn = 0
         self.current_bet = [0,0] # number, rank
 
-    def addPlayer(self,Player):
-        self.players.append(Player)
+    def addPlayer(self,player_name, player_id):
+        if player_name in self.player_names:
+            return False
+        self.player_names.add(player_name)
+        self.players.append(Player(player_name, player_id))
         random.shuffle(self.players)
+        return True
 
-games = set()
+games = {}
 
-@application.route("/start/<gameid>",methods=["GET"])
+@application.route("/start/<gameid>",methods=["GET","POST"])
 def startgame(gameid):
     if request.method == "GET":
         # TODO: start the game
+        pass
 
 @application.route("/config/<gameid>",methods=["GET","POST","DELETE"])
 def config(gameid):
     if request.method == "POST":
         if gameid not in games:
             # Game does not yet exist - create it
-            games.insert(LiarsDiceGame(gameid))
+            games[gameid] = LiarsDiceGame(gameid)
 
         # Now insert this player
         try:
             player_name = request.json['name']
             player_id   = request.json['id']
-            games[gameid].addPlayer(Player(player_name,player_id))
-        except Exception,e:
-            print(e)
+            if not games[gameid].addPlayer(player_name,player_id):
+                return jsonify(status="Error",message="Player name already taken.")
+        except Exception as e:
+            return jsonify(status="Error",message=e)
+
+        return jsonify(status="OK",message="Joined Game")
         
     if request.method == "GET":
-        # Get game state
+        players_status = []
+        for player in games[gameid].players:
+            players_status.append({'name' : player.player_name, 'dice_left' : player.dice_left})
+        return jsonify(players=players_status)
     if request.method == "DELETE":
+        pass
         # Destroy a game
 
 @application.route("/game/<gameid>/<playerid>",methods=['GET','POST','DELETE'])
 def game(gameid,playerid):
     if request.method == "POST":
         # Update game with ID gameid
+        pass
     if request.method == "GET":
-        # Get the game state
+        # TODO:
+        return jsonify(status="OK",message="Game ID:"+str(gameid)+", Player ID:"+str(playerid))
     if request.method == "DELETE":
         # Leave a game
+        pass
 
 if __name__=="__main__":
     application.run(host='0.0.0.0')
